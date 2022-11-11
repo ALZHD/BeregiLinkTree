f0b96628e38df703b300ced169d5b990a2baa5ee
ALZHD <alexanderzhdanovich95@gmail.com>
2022-11-11 18:18:21 +0400

    init: do Release

---
+++ zhdanberegi/src/App.js
@@ -0,0 +1,62 @@
+
+import './App.css';
+import Photo from "./pic/IMG_5191 (1) (1).JPG"
+import Inst from "./pic/pngegg.png"
+import Tel from "./pic/kisspng-telegram-encapsulated-postscript-transfer-5b17060586a686.1271014115282355255515.png"
+function App() {
+  return (
+    <div className="App">
+<div class="container">
+      <div class="containtersmall">
+        <div class="mainphoto">
+          <img src={Photo} alt="myphoto" />
+        </div>
+        <div class="namesbox">
+          <div class="name">Z H D A N</div>
+          <div class="song">Береги</div>
+        </div>
+        <div class="boxforlinks">
+          <div class="instagram">
+            <a href="https://www.instagram.com/zhdan.music/">
+              <img src={Inst} alt="myinst"
+            /></a>
+          </div>
+          <div class="telegram">
+            <a id="links" href="https://t.me/zhdan_music">
+              <img
+                src={Tel}
+                alt="myinst"
+            /></a>
+          </div>
+        </div>
+        <div class="boxforstores">
+          <button
+            onclick="window.location.href = 'https://music.yandex.ru/album/23967780';"
+          >
+            Cлушать на Яндекс.Музыка
+          </button>
+          <button
+            onclick="window.location.href = 'https://open.spotify.com/album/1mFkaY0oPs6nIllqS24dlG';"
+          >
+            Cлушать на Spotify
+          </button>
+          <button
+            onclick="window.location.href = 'https://music.apple.com/ru/album/%D0%B1%D0%B5%D1%80%D0%B5%D0%B3%D0%B8/1653070890?i=1653070900';"
+          >
+            Cлушать на Apple Music
+          </button>
+          <button
+            onclick="window.location.href = 'https://vk.com/artist/zhdan_mty2ode1ntk1mq?z=audio_playlist-2000677433_16677433%2Fda4610324266e7bb28';"
+          >
+            Cлушать на VK Музыка
+          </button>
+        </div>
+        <div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
