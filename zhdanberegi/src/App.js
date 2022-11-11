ed0b45acf6db7d82218cf023dc35b9aa2fb76cde
ALZHD <alexanderzhdanovich95@gmail.com>
2022-11-11 18:53:00 +0400

    init: do Release

--- zhdanberegi/src/App.js
+++ zhdanberegi/src/App.js
@@ -30,19 +30,27 @@ function App() {
           </div>
         </div>
         <div class="boxforstores">
-
-          <form action="https://music.yandex.ru/album/23967780" target="_blank">
-   <button>Cлушать на Яндекс.Музыка</button>
-  </form>
-          <form action="https://open.spotify.com/album/1mFkaY0oPs6nIllqS24dlG" target="_blank">
-   <button>Cлушать на Spotify</button>
-  </form>
-          <form action="https://music.apple.com/ru/album/%D0%B1%D0%B5%D1%80%D0%B5%D0%B3%D0%B8/1653070890?i=1653070900" target="_blank">
-   <button> Cлушать на Apple Music</button>
-  </form>
-          <form action="https://vk.com/artist/zhdan_mty2ode1ntk1mq?z=audio_playlist-2000677433_16677433%2Fda4610324266e7bb28" target="_blank">
-   <button>  Cлушать на VK Музыка</button>
-  </form>
+          <a class="main-link" href="https://music.yandex.ru/album/23967780">
+            Cлушать на "Яндекс.Музыка"
+          </a>
+          <a
+            class="main-link"
+            href="https://open.spotify.com/album/1mFkaY0oPs6nIllqS24dlG"
+          >
+            Cлушать на "Spotify"
+          </a>
+          <a
+            class="main-link"
+            href="https://music.apple.com/ru/album/%D0%B1%D0%B5%D1%80%D0%B5%D0%B3%D0%B8/1653070890?i=1653070900"
+          >
+            Cлушать на "Apple Music"
+          </a>
+          <a
+            class="main-link"
+            href="https://vk.com/artist/zhdan_mty2ode1ntk1mq?z=audio_playlist-2000677433_16677433%2Fda4610324266e7bb28"
+          >
+            Cлушать на "VK Музыка"
+          </a>
         </div>
         <div>
         </div>
